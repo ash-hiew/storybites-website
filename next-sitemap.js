303827f8c9ec64e47886fcca8ea3d70053e7e395
ash-hiew <46409272+ash-hiew@users.noreply.github.com>
2022-05-05 21:42:30 +1200

    Added eslint and prettier for styling

---
+++ next-sitemap.js
@@ -0,0 +1,7 @@
+/* eslint-disable linebreak-style */
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+  siteUrl: process.env.SITE_URL || 'https://example.com',
+  generateRobotsTxt: true,
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,62 +1,63 @@
-import '../styles/globals.css'
+/* eslint-disable require-jsdoc */
+import React from "react";
+import "../styles/globals.css";
 import "../styles/embla.css";
+import { AnimatePresence, motion } from "framer-motion";
+import type { AppProps } from "next/app";
 
+import { DefaultSeo } from "next-seo";
 
-import type { AppProps } from 'next/app'
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-import { AnimatePresence, motion } from 'framer-motion';
-
-import { DefaultSeo } from 'next-seo';
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return( 
-  <div className='bg-stone-200'>
-    <DefaultSeo
-    title='Home'
-    titleTemplate='%s | StoryBites'
-    defaultTitle='StoryBites'
-    description='We are a New Zealand based culinary brand mandated to empower one million people to find humanity in gastronomy.'
-    canonical='https://storybites.co.nz/'
-    openGraph={{
-      url: 'https://storybites.co.nz/',
-      title: 'StoryBites',
-      description: 'We are a New Zealand based culinary brand mandated to empower one million people to find humanity in gastronomy.',
-      images: [
-        {
-          url: '../public/images/og-image.png',
-          width: 1200,
-          height: 630,
-          alt: "StoryBites"
-        },
-      ],
-      type: 'website',
-      locale: 'en_IE',
-      site_name: 'StoryBites',
-    }}
-    />    
-    <Header />
-    <AnimatePresence>
-    <motion.div
-         initial="initial"
-         animate="animate"
-         variants={{
-           initial: {
-             opacity: 0,
-           },
-           animate: {
-             opacity: 1,
-           },
-         }}
-       >
-          
-        <Component {...pageProps} />    
-    </motion.div>
-    </AnimatePresence>
+  return (
+    <div className="bg-stone-200">
+      <DefaultSeo
+        title="Home"
+        titleTemplate="%s | StoryBites"
+        defaultTitle="StoryBites"
+        description="We are a New Zealand based culinary brand mandated to empower one million people to find humanity in gastronomy."
+        canonical="https://storybites.co.nz/"
+        openGraph={{
+          url: "https://storybites.co.nz/",
+          title: "StoryBites",
+          description:
+            "We are a New Zealand based culinary brand mandated to empower one million people to find humanity in gastronomy.",
+          images: [
+            {
+              url: "../public/images/og-image.png",
+              width: 1200,
+              height: 630,
+              alt: "StoryBites",
+            },
+          ],
+          type: "website",
+          locale: "en_IE",
+          site_name: "StoryBites",
+        }}
+      />
+      <Header />
+      <AnimatePresence>
+        <motion.div
+          initial="initial"
+          animate="animate"
+          variants={{
+            initial: {
+              opacity: 0,
+            },
+            animate: {
+              opacity: 1,
+            },
+          }}
+        >
+          <Component {...pageProps} />
+        </motion.div>
+      </AnimatePresence>
 
-    <Footer />
-
-  </div>)
+      <Footer />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
