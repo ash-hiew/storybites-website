bd120cb3b53225a0db8d473fc4a1a6223a6fba6a
ash-hiew <ash.hiew@gmail.com>
2024-05-26 18:43:07 +1200

    Added Spotify social icon

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,6 +6,7 @@ const siteMetadata = {
   email: "hello@storybites.co.nz",
   instagram: "https://www.instagram.com/storybites._/",
   youtube: "https://www.youtube.com/channel/UCuAccpHzrizN1kln0_oGYRQ",
+  spotify: "https://open.spotify.com/show/6rI7ETObNkfgI6Sb87pX2p?si=ae72b7ea6bab4fa5"
 }
 
 export default siteMetadata
